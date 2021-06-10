6dd2abd3df68aaf965537875f2235a2be4b6b45c
Lokendra-2001 <83914759+Lokendra-2001@users.noreply.github.com>
2021-06-10 22:12:17 +0530

    Create Comment in c.c

---
+++ Comment in c.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() 
+{
+    // Write C code here
+    printf("My name is Lokendra Singh I am from Ajmer");
+    /* The definition of above printf function is available in stdio.h header file*/
+        return 0;
+}
