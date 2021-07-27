1038b37293c6ff56be9c7b23c923f4f4ceb6eebd
Lokendra-2001 <83914759+Lokendra-2001@users.noreply.github.com>
2021-07-27 22:03:11 +0530

    Create 4. Constant in program.c

---
+++ 4. Constant in program.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+	//float constant
+	float const pi = 3.14;
+	printf("float = %f\n",pi);
+	
+	//integer constant
+	int const a = 5;
+	printf("int = %d\n",a);
+	
+	//charcter constant
+	char const yes = 'y';
+	printf("char = %c\n",yes);
+	
+	return 0;
